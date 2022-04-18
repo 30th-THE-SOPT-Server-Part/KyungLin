26c1852c4a75932629c54d1650a60f5c3ac7f534
kkl4846 <kkl4846@cau.ac.kr>
2022-04-19 01:43:02 +0900

    [Add] user 클래스 추가, 회원가입post 요청

--- week02/assignment/src/api/signup.ts
+++ week02/assignment/src/api/signup.ts
@@ -1,12 +1,34 @@
 import express, {Request, Response, Router} from 'express';
+import { User,userStorage } from '../models/user';
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request,res: Response) => {
-    return res.status(503).json({
-        status: 503,
-        message: '회원가입 서비스를 이용할 수 없습니다.'
-    });
+router.post('/', (req: Request,res: Response) => {
+
+    const { id, name } = req.body;
+
+    if (id && name) {
+        // 아이디 중복 검사
+        if (userStorage.hasUserId(id)) {
+            return res.status(409).json({
+                status: 409,
+                message: '존재하는 아이디입니다'
+            });
+        } else {
+            const newUser: User = {id: id, name: name};
+            userStorage.addUser(newUser);
+            return res.status(200).json({
+                status: 200,
+                message: '회원가입 완료'
+            });
+        }
+    } else {
+        // 아이디, 이름 기입하지 않았을 때
+        return res.status(404).json({
+            status: 404,
+            message: 'id,name 미기입'
+        }); 
+    }
 });
 
 module.exports = router;
--- week02/assignment/src/api/user.ts
+++ week02/assignment/src/api/user.ts
@@ -1,12 +1,36 @@
 import express, { Request, Response, Router} from 'express';
+import { User, userStorage } from '../models/user';
+
+
 
 const router: Router = express.Router();
 
 router.get('/', (req: Request,res: Response) => {
     return res.status(200).json({
         status: 200,
-        message: 'user 조회 가능'
-    });
+        message: 'user 조회 가능',
+        data: userStorage.users 
+    }) 
 });
+
+router.get('/:userId', (req: Request,res: Response) => {
+
+    const userId: number = req.params.userId as unknown as number;
+    const user: User | undefined = userStorage.getUser(userId)   
+
+    if (user) {
+        return res.status(200).json({
+            status: 200,
+            message: 'user 조회 가능',
+            data: {id: user.id, name: user.name}
+            }) 
+    } else {
+        return res.status(404).json({
+            status: 404,
+            message: '해당 user가 존재하지않습니다'
+            })
+    }
+});
+
 
 module.exports = router;
