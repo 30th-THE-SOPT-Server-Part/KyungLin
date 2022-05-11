1e4deefd0e27d21f30b4ed9d6a6afca8a8c5aeb8
kkl4846 <kkl4846@cau.ac.kr>
2022-05-12 01:53:44 +0900

    [Add] 4주차 실천과제(Levle 1)

---
+++ week04/assignment/Level1/src/interfaces/movie/MovieInfo.ts
@@ -0,0 +1,9 @@
+
+
+export interface MovieInfo {
+    title: string;
+    director: string;
+    startDate: Date;
+    thumbnail: string;
+    story: string;
+}
---
+++ week04/assignment/Level1/src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { UserInfo } from "../interfaces/user/UserInfo";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true  // null이 될 수 없음
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number
+    },
+    school: {
+        name: { type: String },
+        major: { tupe: String }
+    }
+});
+
+export default mongoose.model<UserInfo & mongoose.Document>("User",UserSchema);  //<어떤 type으로>("어떤 이름으로", 어떤 것을 내보낼거임)
