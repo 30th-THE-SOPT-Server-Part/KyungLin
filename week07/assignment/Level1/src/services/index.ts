3c5fc63ab62afd0837b4fb22efad5f5fdebebdc7
kkl4846 <kkl4846@cau.ac.kr>
2022-06-02 03:45:34 +0900

    [ADD] 7주차 실천과제(level1)

---
+++ week07/assignment/Level1/src/services/index.ts
@@ -0,0 +1,6 @@
+import UserService from "./UserService";
+import FileService from "./FileService";
+import MovieService from "./MovieService";
+import ReviewService from "./ReviewService";
+//service index file
+export { UserService, FileService, MovieService, ReviewService };
